50a62c307c0f0601e2a7e6eafc23f4e8ff40a7ac
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-07-25 18:51:24 +0530

    -

--- assemble.ps1
+++ assemble.ps1
@@ -9,10 +9,10 @@ mkdir -p dflat\linker
 git clone --depth 1 -b main https://github.com/dotnet/roslyn 
 rm roslyn\src\Compilers\CSharp\csc\AnyCpu\csc.csproj
 cp csc.csproj roslyn\src\Compilers\CSharp\csc\AnyCpu\csc.csproj
-roslyn\build.cmd
+roslyn\restore.cmd
 
-cp roslyn\artifacts\bin\csc\Release\net<>\win-x64\publish\csc.exe dflat\csc\csc.exe
+# cp roslyn\artifacts\bin\csc\Release\net<>\win-x64\publish\csc.exe dflat\csc\csc.exe
 
 # build ilc, runtime (dotnet\runtime)
-git clone --depth 1 -b main https://github.com/dotnet/runtime
-runtime\build.cmd clr.nativeaotlibs+clr.nativeaotruntime+clr.alljits+clr.tools+libs -rc Release -lc Release
+# git clone --depth 1 -b main https://github.com/dotnet/runtime
+# runtime\build.cmd clr.nativeaotlibs+clr.nativeaotruntime+clr.alljits+clr.tools+libs -rc Release -lc Release
