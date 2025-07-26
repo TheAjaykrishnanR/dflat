619c7bec891aa5ca8687fca5afe138b631e053ae
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-07-26 16:02:42 +0530

    -

--- assemble.ps1
+++ assemble.ps1
@@ -12,7 +12,7 @@ mkdir -p build\libs\runtime
 # roslyn\restore.cmd
 # roslyn\.dotnet\dotnet.exe publish roslyn\src\Compilers\CSharp\csc\AnyCpu\csc.csproj 
 # 
-# cp roslyn\artifacts\bin\csc\Release\net9.0\win-x64\publish\csc.exe build\csc\csc.exe
+# cp roslyn\artifacts\bin\csc\Release\net*\win-x64\publish\csc.exe build\csc\csc.exe
 
 # build ilc, runtime, libs (dotnet\runtime)
 # git clone --depth 1 -b main https://github.com/dotnet/runtime
@@ -21,8 +21,8 @@ mkdir -p build\libs\runtime
 # $coreclr = "runtime\artifacts\bin\coreclr\windows.x64.Release\"
 # cp "$coreclr\aotsdk\*" build\libs\aotsdk\
 # cp "$coreclr\x64\ilc\*" build\ilc\
-# cp runtime\artifacts\bin\microsoft.netcore.app.ref\ref\net10.0\* build\libs\refs
-# cp runtime\artifacts\bin\microsoft.netcore.app.runtime.win-x64\Release\runtimes\win-x64\lib\net10.0\* build\libs\runtime
+# cp runtime\artifacts\bin\microsoft.netcore.app.ref\ref\net*\* build\libs\refs
+# cp runtime\artifacts\bin\microsoft.netcore.app.runtime.win-x64\Release\runtimes\win-x64\lib\net*\* build\libs\runtime
 # cp runtime\src\coreclr\nativeaot\BuildIntegration\WindowsAPIs.txt build\libs\WindowsAPIs.txt
 # 
 # Remove-Item -Recurse -Force -Confirm:$false "build\libs\aotsdk\*.xml"
@@ -36,16 +36,16 @@ mkdir -p build\libs\runtime
 curl -Lo llvm.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-pc-windows-msvc.tar.xz
 mkdir llvm
 & "C:\Program Files\Git\usr\bin\tar.exe" -xvf llvm.tar.xz -C llvm
-cp llvm\bin\lld-link.exe build\linker\lld-link.exe
+cp llvm\*\bin\lld-link.exe build\linker\lld-link.exe
 
 # compile dflat.cs
-# cp compile.ps1 build\compile.ps1
-# cp dflat.cs build\dflat.cs
-# cd build
-# compile.ps1 dflat.cs
-# rm compile.ps1
-# rm dflat.cs
-# cd ..
-# 
-# # package
-# tar -czvf dflat.tar.gz .\build
+cp compile.ps1 build\compile.ps1
+cp dflat.cs build\dflat.cs
+cd build
+compile.ps1 dflat.cs
+rm compile.ps1
+rm dflat.cs
+cd ..
+
+# pack
+& "C:\Program Files\Git\usr\bin\tar.exe" -czvf dflat.tar.gz .\build
