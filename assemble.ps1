cf21286977297ab5c63218d652ea2c91588e288c
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-07-26 15:24:35 +0530

    -

--- assemble.ps1
+++ assemble.ps1
@@ -35,7 +35,8 @@ mkdir -p build\libs\runtime
 # lld-link (llvm)
 # curl -Lo llvm.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-pc-windows-msvc.tar.xz
 # mkdir llvm
-winget install -e --id 7zip.7zip
+curl -Lo 7zip.exe https://www.7-zip.org/a/7zr.exe
+.\7zip.exe
 # cp llvm\bin\lld-link.exe build\linker\lld-link.exe
 
 # compile dflat.cs
