d535ffa4f79ff819fff38f37bc81dec7ec0f21f2
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-07-26 22:04:33 +0530

    -

--- .github/test.ps1
+++ .github/test.ps1
@@ -1,2 +1,2 @@
-curl -Lo vs-downloader.py https://gist.github.com/mmozeiko/7f3162ec2988e81e56d5c4e22cde9977/raw/50d5f534519bce8c118df3e2a2bc9a6f16e29a58/portable-msvc.py
+curl -Lo vs-downloader.py https://gist.github.com/TheAjaykrishnanR/1ed9254e7bf20bfbabb667124e331d21/raw/b3f026554d2a646a28c0a74dd24dbd4a6f15eb2f/portable-msvc.py
 python vs-downloader.py --sdk-version 26100
