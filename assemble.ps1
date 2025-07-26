f3c1e827d034f28f322d9cbc6d3cc9bd4212ff39
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-07-26 14:17:11 +0530

    -

--- assemble.ps1
+++ assemble.ps1
@@ -36,7 +36,7 @@ mkdir -p build\libs\runtime
 curl -Lo llvm.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-pc-windows-msvc.tar.xz
 mkdir llvm
 which tar
-tar -xvf llvm.tar.xz
+tar -xvf "llvm.tar.xz" -C "llvm"
 cp llvm\bin\lld-link.exe build\linker\lld-link.exe
 
 # compile dflat.cs
